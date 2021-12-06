1bca3b431e8eb6e59472ca8d1823138b11d31a51
Kevin Restaino <kevinrestaino@gmail.com>
2021-12-05 19:32:07 -0800

    refactor: use uppercase for variables

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # get flags
 while getopts 'f' flag; do
   case "${flag}" in
-    f) no_cache='true' ;;
+    f) NO_CACHE='true' ;;
   esac
 done
 
@@ -9,40 +9,40 @@ done
 rm -rf build/*
 
 # set theme
-theme="system"
-echo "Building for $theme theme"
+THEME="system"
+echo "Building for $THEME theme"
 
-dark_folder="FolderDark512x512@2x.png"
-light_folder="Folder512x512@2x.png"
+DARK_FOLDER="FolderDark512x512@2x.png"
+LIGHT_FOLDER="Folder512x512@2x.png"
 
-if [ $theme == "system" ]; then
+if [ $THEME == "system" ]; then
   if [ "$(defaults read -g AppleInterfaceStyle)" == "Dark" ]; then
-    theme_folder_file=$dark_folder
+    THEME_FOLDER_FILE=$DARK_FOLDER
   else
-    theme_folder_file=$light_folder
+    THEME_FOLDER_FILE=$LIGHT_FOLDER
   fi
 else
-  if [ "$theme" = "dark" ]; then
-    theme_folder_file=$dark_folder
+  if [ "$THEME" = "dark" ]; then
+    THEME_FOLDER_FILE=$DARK_FOLDER
   else
-    theme_folder_file=$light_folder
+    THEME_FOLDER_FILE=$LIGHT_FOLDER
   fi
 fi
 
 # extract blank folder from OS assets
-assets_location="/System/Library/PrivateFrameworks/IconFoundation.framework/Versions/A/Resources/Assets.car"
-temp=$TMPDIR/io.kmr.folderIcons
-folder_icon="$temp/$theme_folder_file"
+ASSETS_LOCATION="/System/Library/PrivateFrameworks/IconFoundation.framework/Versions/A/Resources/Assets.car"
+TEMP=$TMPDIR/io.kmr.folderIcons
+FOLDER_ICON="$TEMP/$THEME_FOLDER_FILE"
 
-if [[ $no_cache ]]; then
-  rm -rf $temp
+if [[ $NO_CACHE ]]; then
+  rm -rf $TEMP
 fi
 
-if test -f "$folder_icon"; then
+if test -f "$FOLDER_ICON"; then
   echo "Blank folder already extracted, skipping..."
 else
   echo "Extracting blank folder"
-  ./bin/acextract -i $assets_location -o $temp > /dev/null
+  ./bin/acextract -i $ASSETS_LOCATION -o $TEMP > /dev/null
 fi
 
 # convert svgs
@@ -52,44 +52,44 @@ mogrify -density 2000 -resize x512 -background transparent -format png -path cus
 # build custom icon folders
 echo "Building custom icons"
 for file in custom/*.png; do
-  filename=$(echo "$file" | cut -f 1 -d '.')
-  basename="$(basename "$filename")"
+  FILENAME=$(echo "$file" | cut -f 1 -d '.')
+  BASENAME="$(BASENAME "$FILENAME")"
 
-  convert $folder_icon \
+  convert $FOLDER_ICON \
     \( -background transparent -size 512x512 -gravity center -geometry +0+40 $file -resize X300 -colorize 100 -fill '#1ca1dd' +opaque "#00000000" \) \
-    -compose over -composite "build/$basename.png"
+    -compose over -composite "build/$BASENAME.png"
 done
 
 # build SF Pro icon folders
 echo "Building symbol icons"
-symbols=$(cat symbols.txt)
+SYMBOLS=$(cat symbols.txt)
 
-for ((i = 0; i < ${#symbols}; i++)); do
-  symbol="${symbols:$i:1}"
-  convert $folder_icon \
-    \( -background transparent -fill '#1ca1dd' -font SF-Pro-Text-Regular -size 512x512 -pointsize 340 -gravity center -geometry +0+40 label:$symbol \) \
-    -compose over -composite build/$symbol.png
+for ((i = 0; i < ${#SYMBOLS}; i++)); do
+  SYMBOL="${SYMBOLS:$i:1}"
+  convert $FOLDER_ICON \
+    \( -background transparent -fill '#1ca1dd' -font SF-Pro-Text-Regular -size 512x512 -pointsize 340 -gravity center -geometry +0+40 label:$SYMBOL \) \
+    -compose over -composite build/$SYMBOL.png
 done
 
 # build
 echo "Building .icns files"
 for file in build/*.png; do
-  filename=$(echo "$file" | cut -f 1 -d '.')
-  mkdir "$filename.iconset"
+  FILENAME=$(echo "$file" | cut -f 1 -d '.')
+  mkdir "$FILENAME.iconset"
 
-  sips -z 16 16 "$filename.png" --out "$filename.iconset/icon_16x16.png" > /dev/null
-  sips -z 32 32 "$filename.png" --out "$filename.iconset/icon_16x16@2x.png" > /dev/null
-  sips -z 32 32 "$filename.png" --out "$filename.iconset/icon_32x32.png" > /dev/null
-  sips -z 64 64 "$filename.png" --out "$filename.iconset/icon_32x32@2x.png" > /dev/null
-  sips -z 128 128 "$filename.png" --out "$filename.iconset/icon_128x128.png" > /dev/null
-  sips -z 256 256 "$filename.png" --out "$filename.iconset/icon_128x128@2x.png" > /dev/null
-  sips -z 256 256 "$filename.png" --out "$filename.iconset/icon_256x256.png" > /dev/null
-  sips -z 512 512 "$filename.png" --out "$filename.iconset/icon_256x256@2x.png" > /dev/null
-  sips -z 512 512 "$filename.png" --out "$filename.iconset/icon_512x512.png" > /dev/null
-  cp "$filename.png" "$filename.iconset/icon_512x512@2x.png"
+  sips -z 16 16 "$FILENAME.png" --out "$FILENAME.iconset/icon_16x16.png" > /dev/null
+  sips -z 32 32 "$FILENAME.png" --out "$FILENAME.iconset/icon_16x16@2x.png" > /dev/null
+  sips -z 32 32 "$FILENAME.png" --out "$FILENAME.iconset/icon_32x32.png" > /dev/null
+  sips -z 64 64 "$FILENAME.png" --out "$FILENAME.iconset/icon_32x32@2x.png" > /dev/null
+  sips -z 128 128 "$FILENAME.png" --out "$FILENAME.iconset/icon_128x128.png" > /dev/null
+  sips -z 256 256 "$FILENAME.png" --out "$FILENAME.iconset/icon_128x128@2x.png" > /dev/null
+  sips -z 256 256 "$FILENAME.png" --out "$FILENAME.iconset/icon_256x256.png" > /dev/null
+  sips -z 512 512 "$FILENAME.png" --out "$FILENAME.iconset/icon_256x256@2x.png" > /dev/null
+  sips -z 512 512 "$FILENAME.png" --out "$FILENAME.iconset/icon_512x512.png" > /dev/null
+  cp "$FILENAME.png" "$FILENAME.iconset/icon_512x512@2x.png"
 
-  iconutil -c icns "$filename.iconset"
-  rm -R "$filename.iconset"
+  iconutil -c icns "$FILENAME.iconset"
+  rm -R "$FILENAME.iconset"
 done
 
 # cleanup build
